01cfaf0985dfaf6dbc6100af1d902849751078fd
garethhallberg <gareth@ghallberg.co.uk>
2022-11-08 16:47:16 +0000

    url for image

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ import 'tachyons'
 function Card({id, name, username}) {
     return (
       <div className="bg-light-green dib br3 pa3 ma2 grow bw2shadow-15" >
-        <img alt='robots' src={`https:robohash.org/${username}?200x200`} />
+        <img alt='robots' src={`https://robohash.org/${username}?200x200`} />
         <h1 className='f4'>{name}</h1>
-        <p>Welcome to the dome of truth user with id of {id}</p>
+        <p>Welcome to the home of truth user with id of {id}</p>
       </div>
     );
   }
